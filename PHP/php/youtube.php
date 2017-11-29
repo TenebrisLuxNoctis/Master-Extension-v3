d0cddf79979decd876fb3a4ee25f81732c5f5529
TenebrisLuxNoctis <30625135+TenebrisLuxNoctis@users.noreply.github.com>
2017-11-30 00:53:14 +0100

    Correctif fichier youtube.php

--- PHP/php/youtube.php
+++ PHP/php/youtube.php
@@ -5,12 +5,12 @@
 	if(isset($_GET['lasttime']) && $_GET['lasttime'] != "")
 	{
 		$datetime = new DateTime($_GET['lasttime']);
-		$cusor = "&amp;publishedAfter=".$datetime->format(DateTime::ATOM);
+		$cusor = "&publishedAfter=".$datetime->format(DateTime::ATOM);
 	}
-    $channelID	= "UCOhP0t6arWMXqmcroJjMJ7A"
-    $token		= 'AIzaSyAANw33DQWRi5OlPqgtRum_oQGWjB_BXdA';
+    $channelID	= "UCOhP0t6arWMXqmcroJjMJ7A";
+    $token	= 'AIzaSyAANw33DQWRi5OlPqgtRum_oQGWjB_BXdA';
 
-	$url = "https://www.googleapis.com/youtube/v3/search?part=snippet&amp;channelId=".$channelID."&order=date&amp;key=".$token."&amp;maxResults=1".$cursor;
+	$url = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=".$channelID."&order=date&key=".$token."&maxResults=1".$cursor;
     
 	//Récupération des informations depuis l'API
     $stream = file_get_contents($url);
